b02140da8d8e96f99cce473e0b665baa60af7502
CllaudiaB <buraanna27@gmail.com>
2025-01-29 10:37:35 +0100

    task 0

---
+++ making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Make change"""
+
+
+def makeChange(coins, total):
+    """Given a pile of coins of different values,
+       determine the fewest number of coins needed
+       to meet a given amount total"""
+
+    coins.sort(reverse=True)
+    
+    count = 0
+    for coin in coins:
+        count += total // coin
+        total %= coin
+
+    if total == 0:
+        return count
+    return -1
